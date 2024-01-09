e6db3a4dead7c03b5a33d969c044bf100c3fdd9a
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2024-01-09 21:10:18 +0600

    fixed logic

--- js/index.js
+++ js/index.js
@@ -1,12 +1,11 @@
 function checkAge() {
-    const age = document.getElementById('age').value;
+  const age = document.getElementById('age').value;
 
-    if (age >= 18) {
-      $('#adultModal').modal('show');
-    } else if (age <= 18 && age===1) {
-      $('#childModal').modal('show');
-    }
-    else{
-        $('#errorModal').modal('show');
-    }
+  if (age >= 18) {
+    $('#adultModal').modal('show');
+  } else if (age >= 1 && age < 18) {
+    $('#childModal').modal('show');
+  } else {
+    $('#errorModal').modal('show');
   }
+}
